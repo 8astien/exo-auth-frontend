ef03f2f01c7952456f1f0c68f7d189118c134747
8astien <bastien.cauquil@pm.me>
2022-06-23 09:56:54 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Hero from './Components/Hero/Hero';
+import Band from './Components/Band/Band';
+import Desc from './Components/Desc/Desc';
+import Footer from './Components/Footer/Footer';
+import BandForm from './Components/BandForm/BandForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Hero />
+      <Band />
+      <Desc />
+      <BandForm 
+        value = 'Acheter'
+      />
+      <Footer />
+    </>
   );
 }
 
